63e88224416bd10a8e0245878927a9346dc90b1d
Kornel Lugosi <coornail@gmail.com>
2014-03-06 13:55:02 +0100

    Implement cross-browser support for settings storage

--- src/chrome/popup.js
+++ src/chrome/popup.js
@@ -1,15 +1,39 @@
 (function ($) {
   "use strict";
+
+  var settingCallback = function(setting, callback) {
+    if (chrome) {
+      chrome.storage.sync.get(setting, function (items) {
+        callback(items[setting]);
+      });
+    } else {
+      var storage = require("sdk/simple-storage");
+      callback(storage[setting]);
+    }
+  },
+
+    saveSetting = function(key, value) {
+      if (chrome) {
+        chrome.storage.sync.set({key: value});
+      } else {
+        var storage = require("sdk/simple-storage");
+
+        storage[key] = value;
+      }
+    };
+
   $(document).ready(function () {
     // Load checkbox state.
-    chrome.storage.sync.get('client-js-enabled', function (items) {
-      $('#client-js-enabled').prop('checked', items['client-js-enabled']);
+    settingCallback('client-js-enabled', function (setting) {
+      $('#client-js-enabled').prop('checked', setting);
     });
+
+    // @Todo get walkhub origin from the storage.
 
     // Save checkbox state.
     $('#client-js-enabled').on('change', function () {
       var client_js_enabled = $(this).is(':checked');
-      chrome.storage.sync.set({'client-js-enabled': client_js_enabled});
+      saveSetting('client-js-enabled', client_js_enabled);
     });
   });
 })(jQuery);
