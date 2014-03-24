1db37267a7f30a753c4fa366244fca91f60b9c79
Kornel Lugosi <coornail@gmail.com>
2014-03-24 11:26:29 +0100

    Fix walkthrough playing bug

--- src/chrome/walkhub_loader.js
+++ src/chrome/walkhub_loader.js
@@ -11,21 +11,23 @@
    * Load walkhub resources.
    */
   function load_walkhub_resources() {
+    // Do not load javascript in the parent iframe.
+    if (window.location.pathname === '/walkhub') {
+      return;
+    }
+
     var compiled_js = document.createElement('script');
     compiled_js.src = walkhub_origin + '/resources/compiled.js';
-
     attach_to_document(compiled_js);
 
     var walkhub_origin_js = document.createElement('script');
-    var origin_json = document.createTextNode('window.Walkhub = window.Walkhub || {}; window.Walkhub.ProxyOrigin = function () { return "' + walkhub_origin + '"; };');
+    var origin_json = document.createTextNode('window.Walkhub = window.Walkhub || {}; Walkhub.Origin = function () { return "' + walkhub_origin +'";}');
     walkhub_origin_js.appendChild(origin_json);
-
     attach_to_document(walkhub_origin_js);
 
     var stylesheet = document.createElement('link');
     stylesheet.href = walkhub_origin + '/resources/joyride.css';
     stylesheet.rel = 'stylesheet';
-
     attach_to_document(stylesheet);
   }
 
