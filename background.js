5ef098cc190fb46bd952da2d51dc84939bbf3f31
Tamás Demeter-Haludka <yorirou@gmail.com>
2016-12-12 16:07:25 +0100

    Version update.

--- background.js
+++ background.js
@@ -31,7 +31,6 @@ function addWalkhubCSP(headerValue) {
 }
 
 chrome.webRequest.onHeadersReceived.addListener(function (details) {
-	console.log(details);
 	var headers = details.responseHeaders;
 	for (var i = 0; i < headers.length; i++) {
 		if (headers[i].name === "Content-Security-Policy") {
